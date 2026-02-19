2c04b6595e89851f052914a1e1362fc4c156c371
Benzarti <ahmedyassinebenzarti@gmail.com>
2026-02-19 02:00:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1892302408-8f785",
+  "appId": "1:811812832049:web:9c29fd551b3876ab18e610",
+  "apiKey": "AIzaSyAfe1yAsHi5gHCxDPPk0tJyP-Y5D7KBF28",
+  "authDomain": "studio-1892302408-8f785.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "811812832049"
+};
