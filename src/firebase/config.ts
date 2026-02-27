a2f217001b892295082d5f5bf09a110e43b010dd
Benzarti <ahmedyassinebenzarti@gmail.com>
2026-02-27 19:20:37 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,13 @@
 /**
- * @fileOverview Configuration Firebase (Valeurs en dur).
- * Note : Pour plus de sécurité, il est recommandé d'utiliser des variables d'environnement.
+ * @fileOverview Configuration Firebase officielle pour le projet TempAlert.
+ * Note : Les clés sont maintenant intégrées directement pour faciliter le build.
  */
 
 export const firebaseConfig = {
-  apiKey: "AIzaSy... (votre_api_key)",
-  authDomain: "tempalert-enim.firebaseapp.com",
-  projectId: "tempalert-enim",
-  storageBucket: "tempalert-enim.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456"
+  apiKey: "AIzaSyAfe1yAsHi5gHCxDPPk0tJyP-Y5D7KBF28",
+  authDomain: "studio-1892302408-8f785.firebaseapp.com",
+  projectId: "studio-1892302408-8f785",
+  storageBucket: "studio-1892302408-8f785.firebasestorage.app",
+  messagingSenderId: "811812832049",
+  appId: "1:811812832049:web:9c29fd551b3876ab18e610"
 };
